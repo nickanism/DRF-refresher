885bc7643224b089dfdbc8639512719ffbfac52e
Nick <nickanism@gmail.com>
2020-12-30 14:51:30 +0900

    WIP

--- products/views.py
+++ products/views.py
@@ -1,16 +1,24 @@
-from django.views.generic.detail import DetailView
-from django.views.generic.list import ListView
+# from django.views.generic.detail import DetailView
+# from django.views.generic.list import ListView
 
+from django.http import JsonResponse
 from .models import Product, Manufacturer
 
-class ProductDetailView(DetailView):
-    model = Product
-    template_name = "products/product_detail.html"
 
+def product_list(request):
+    products = Product.objects.all()
+    data = {"products": list(products.values("pk", "name"))}
+    response = JsonResponse(data)
+    return response
 
-class ProductListView(ListView):
-    model = Product
-    template_name = "products/product_list.html"
+# class ProductDetailView(DetailView):
+#     model = Product
+#     template_name = "products/product_detail.html"
+#
+#
+# class ProductListView(ListView):
+#     model = Product
+#     template_name = "products/product_list.html"
 
 
 
