6dbf11b3f9896edeff56f0b937f41a49fae13a06
Nick <nickanism@gmail.com>
2020-12-30 15:02:48 +0900

    WIP

--- products/views.py
+++ products/views.py
@@ -7,8 +7,33 @@ from .models import Product, Manufacturer
 
 def product_list(request):
     products = Product.objects.all()
-    data = {"products": list(products.values("pk", "name"))}
+    data = {"products": list(products.values())}
     response = JsonResponse(data)
+    return response
+
+def product_detail(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+        data = {"product": {
+            "name": product.name,
+            "manufacturer": product.manufacturer.name,
+            "description": product.description,
+            "photo": product.photo.url,
+            "price": product.price,
+            "shipping_cost": product.photo.url,
+            "quantity": product.quantity,
+        }}
+
+        response = JsonResponse(data)
+
+    except Product.DoesNotExist:
+        response = JsonResponse({
+            "error": {
+                "code": 404,
+                "message": "product not found!"
+            }
+        }, status=404)
+
     return response
 
 # class ProductDetailView(DetailView):
